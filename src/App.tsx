71eef27b12deff65e1f50262df62bca38a05b33f
janosnagygyorgy <161525672+janosnagygyorgy@users.noreply.github.com>
2025-06-30 18:49:10 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+
 function App() {
   return (
     <>
-      <h1>Kreditindex kalkulátor</h1>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<Navigate replace to="/home" />} />
+        </Routes>
+      </div>
     </>
   );
 }
