3437bd1b506970aac7258d7bf264753e1bb27cee
janosnagygyorgy <161525672+janosnagygyorgy@users.noreply.github.com>
2025-07-02 00:05:03 +0200

    Fix credit and grade input limits

--- src/components/SubjectListInput.tsx
+++ src/components/SubjectListInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { v4 } from "uuid";
 import type { Subject } from "../interfaces/subject";
+import { limit } from "../utils/utils";
 
 interface SubjectListInputProps {
   onAddSubject: (subject: Subject) => void;
@@ -9,8 +10,9 @@ interface SubjectListInputProps {
 function SubjectListInput({ onAddSubject }: SubjectListInputProps) {
   const [inputCompleted, setInputCompleted] = useState(false);
   const [inputName, setInputName] = useState("");
-  const [inputCredit, setInputCredit] = useState("3");
-  const [inputGrade, setInputGrade] = useState("5");
+  const [inputCredit, setInputCredit] = useState(3);
+  const [inputGrade, setInputGrade] = useState(5);
+
   return (
     <>
       <input
@@ -26,15 +28,19 @@ function SubjectListInput({ onAddSubject }: SubjectListInputProps) {
       <input
         type="number"
         min={0}
-        value={inputCredit}
-        onChange={(event) => setInputCredit(event.target.value)}
+        value={inputCredit.toString()}
+        onChange={(event) => setInputCredit(Number(event.target.value))}
       />
       <input
         type="number"
         min={1}
         max={5}
-        value={inputGrade}
-        onChange={(event) => setInputGrade(event.target.value)}
+        value={inputGrade.toString()}
+        onChange={(event) =>
+          setInputGrade(
+            limit(Number(event.target.value.replace(/^[1-5]/, "")), 1, 5)
+          )
+        }
       />
       <input
         type="button"
