021cc5d23ea1ab12d2cfcae47dd97f6502aebed2
janosnagygyorgy <161525672+janosnagygyorgy@users.noreply.github.com>
2025-07-01 20:46:05 +0200

    Add SubjectListInput

--- src/components/SubjectList.tsx
+++ src/components/SubjectList.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 import type { Subject } from "../interfaces/subject";
 import SubjectListItem from "./SubjectListItem";
-import { v4 } from "uuid";
+import SubjectListInput from "./SubjectListInput";
 
 interface SubjectListProps {
   subjects: Subject[];
@@ -16,49 +15,10 @@ function SubjectList({
   onUpdateSubject,
   onDeleteSubject,
 }: SubjectListProps) {
-  const [inputCompleted, setInputCompleted] = useState(false);
-  const [inputName, setInputName] = useState("");
-  const [inputCredit, setInputCredit] = useState("3");
-  const [inputGrade, setInputGrade] = useState("5");
   return (
     <ul>
       <li>
-        <input
-          type="checkbox"
-          checked={inputCompleted}
-          onChange={(event) => setInputCompleted(event.target.checked)}
-        />
-        <input
-          type="text"
-          value={inputName}
-          onChange={(event) => setInputName(event.target.value)}
-        />
-        <input
-          type="number"
-          min={0}
-          value={inputCredit}
-          onChange={(event) => setInputCredit(event.target.value)}
-        />
-        <input
-          type="number"
-          min={1}
-          max={5}
-          value={inputGrade}
-          onChange={(event) => setInputGrade(event.target.value)}
-        />
-        <input
-          type="button"
-          value="Add"
-          onClick={() => {
-            onAddSubject({
-              id: v4(),
-              completed: inputCompleted,
-              name: inputName,
-              credit: Number(inputCredit),
-              grade: Number(inputGrade),
-            });
-          }}
-        />
+        <SubjectListInput onAddSubject={onAddSubject} />
       </li>
       {subjects.map((subject) => (
         <li key={subject.id}>
