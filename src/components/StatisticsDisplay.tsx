f780fe0cc4e2f60ee5262956c3a3cb25a3b9bcec
janosnagygyorgy <161525672+janosnagygyorgy@users.noreply.github.com>
2025-07-08 13:56:33 +0200

    Add semester statistics

--- src/components/StatisticsDisplay.tsx
+++ src/components/StatisticsDisplay.tsx
@@ -7,7 +7,30 @@ interface StatisticsDisplayProps {
 function StatisticsDisplay({ calculatorService }: StatisticsDisplayProps) {
   return (
     <>
-      <div>Kreditek összege: {calculatorService.creditSum().toString()}</div>
+      <h2>Féléves adatok</h2>
+      <div>
+        Hagyományos átlag:
+        {Math.round(calculatorService.semesterTraditionalAverage() * 100) / 100}
+      </div>
+      <div>
+        Súlyozott átlag:
+        {Math.round(calculatorService.semesterWeightedAverage() * 100) / 100}
+      </div>
+      <div>Felvett kredit: {calculatorService.semesterCreditSum()}</div>
+      <div>
+        Teljesített kredit: {calculatorService.semesterCompletedCreditSum()}
+      </div>
+      <div>
+        Kreditindex:{" "}
+        {Math.round(calculatorService.semesterCreditIndex() * 100) / 100}
+      </div>
+      <div>
+        Korrigált kreditindex:
+        {Math.round(calculatorService.semesterCorrectedCreditIndex() * 100) /
+          100}
+      </div>
+
+      <h2>Összesített adatok</h2>
     </>
   );
 }
--- src/services/CalculatorService.ts
+++ src/services/CalculatorService.ts
@@ -9,19 +9,92 @@ class CalculatorService {
     this.selectedSemester = selectedSemester;
   }
 
-  public creditSum(): number {
-    return this.data[this.selectedSemester].reduce(
-      (acc, curr) => acc + curr.credit,
+  //#region Semester statistics
+  //#region Sums
+  public semesterGradeSum(semester: string = this.selectedSemester): number {
+    return this.data[semester].reduce((acc, curr) => acc + curr.grade, 0);
+  }
+
+  public semesterCompletedGradeSum(
+    semester: string = this.selectedSemester
+  ): number {
+    return this.data[semester]
+      .filter((e) => e.completed && e.grade > 1)
+      .reduce((acc, curr) => acc + curr.grade, 0);
+  }
+
+  public semesterCreditGradeProductSum(
+    semester: string = this.selectedSemester
+  ): number {
+    return this.data[semester].reduce(
+      (acc, curr) => acc + curr.credit * curr.grade,
       0
     );
   }
 
-  public gradeSum(): number {
-    return this.data[this.selectedSemester].reduce(
-      (acc, curr) => acc + curr.grade,
-      0
+  public semesterCompletedCreditGradeProductSum(
+    semester: string = this.selectedSemester
+  ): number {
+    return this.data[semester]
+      .filter((e) => e.completed && e.grade > 1)
+      .reduce((acc, curr) => acc + curr.credit * curr.grade, 0);
+  }
+
+  public semesterNumberOfSubjects(
+    semester: string = this.selectedSemester
+  ): number {
+    return this.data[semester].length;
+  }
+  //#endregion Sums
+
+  // Hagyományos átlag
+  public semesterTraditionalAverage(): number {
+    return this.semesterGradeSum() / this.semesterNumberOfSubjects();
+  }
+
+  // Súlyozott átlag
+  public semesterWeightedAverage(): number {
+    return (
+      this.semesterCompletedCreditGradeProductSum() /
+      this.semesterCompletedCreditSum()
     );
   }
+
+  // Felvett kredit
+  public semesterCreditSum(semester: string = this.selectedSemester): number {
+    return this.data[semester].reduce((acc, curr) => acc + curr.credit, 0);
+  }
+
+  // Teljesített kredit
+  public semesterCompletedCreditSum(
+    semester: string = this.selectedSemester
+  ): number {
+    return this.data[semester]
+      .filter((e) => e.completed && e.grade > 1)
+      .reduce((acc, curr) => acc + curr.credit, 0);
+  }
+
+  // Kreditindex
+  public semesterCreditIndex(): number {
+    return this.semesterCompletedCreditGradeProductSum() / 30;
+  }
+
+  // Korrigált kreditindex
+  public semesterCorrectedCreditIndex(): number {
+    return (
+      this.semesterCreditIndex() *
+      (this.semesterCompletedCreditSum() / this.semesterCreditSum())
+    );
+  }
+  //#endregion Semester statistics
+
+  //#region Cumulated statistics
+  // Összesített hagyományos átlag
+  // Összesített súlyozott átlag
+  // Összesített kreditindex
+  // Összesített korrigált kreditindex
+  // Összes teljesített kredit
+  //#endregion Cumulated statistics
 }
 
 export default CalculatorService;
