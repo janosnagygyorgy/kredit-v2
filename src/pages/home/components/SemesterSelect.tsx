25af8ad96c700edc6b0f5cbac68490692bcaed80
janosnagygyorgy <161525672+janosnagygyorgy@users.noreply.github.com>
2025-07-20 17:43:49 +0200

    Make semester list collapsible

--- src/pages/home/components/SemesterSelect.tsx
+++ src/pages/home/components/SemesterSelect.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import type { DraggableItem } from "components/DragDropList/DraggableItem";
 import DragDropList from "components/DragDropList/DragDropList";
 
@@ -20,46 +20,61 @@ function SemesterSelect({
   onMoveSemester,
 }: SemesterSelectProps) {
   const addSemesterInput = useRef<HTMLInputElement>(null);
+  const [active, setActive] = useState(true);
 
   function handleSemesterChange(semester: string) {
     onChangeSelectedSemester(semester);
   }
 
   return (
-    <div className="w-1/2">
-      <DragDropList
-        onMoveItem={onMoveSemester}
-        items={options.map(
-          (option, index) =>
-            ({
-              key: option,
-              children: (
-                <div
-                  onClick={() => handleSemesterChange(option)}
-                  className={`cursor-pointer active:cursor-grabbing ${
-                    option === selectedSemester ? "bg-red-300" : ""
-                  }`}
-                >
-                  {`${index}. ${option}`}
-                </div>
-              ),
-            } as DraggableItem)
-        )}
+    <div className="w-1/2 p-2 border-1 border-solid rounded-sm">
+      <div
+        className="bg-gray-200 select-none"
+        onClick={() => setActive(() => !active)}
+      >
+        {selectedSemester}
+      </div>
+      <div
+        className="grid transition-all duration-300 ease-in-out"
+        style={{ gridTemplateRows: active ? "1fr" : "0fr" }}
+      >
+        <div className="overflow-hidden">
+          <DragDropList
+            onMoveItem={onMoveSemester}
+            items={options.map(
+              (option, index) =>
+                ({
+                  key: option,
+                  children: (
+                    <div
+                      onClick={() => handleSemesterChange(option)}
+                      className={`cursor-pointer active:cursor-grabbing ${
+                        option === selectedSemester ? "bg-red-300" : ""
+                      }`}
+                    >
+                      {`${index}. ${option}`}
+                    </div>
+                  ),
+                } as DraggableItem)
+            )}
+          />
+        </div>
+      </div>
+      <input
+        type="button"
+        value="Félév törlése"
+        onClick={() => onDeleteSemester(selectedSemester)}
       />
-      <button onClick={() => onDeleteSemester(selectedSemester)}>
-        Félév törlése
-      </button>
-      <div>{selectedSemester}</div>
       <input type="text" ref={addSemesterInput} />
-      <button
+      <input
+        type="button"
+        value="Félév hozzáadása"
         onClick={() => {
           if (!addSemesterInput.current) return;
           onAddSemester(addSemesterInput.current.value);
           addSemesterInput.current.value = "";
         }}
-      >
-        Félév hozzáadása
-      </button>
+      />
     </div>
   );
 }
